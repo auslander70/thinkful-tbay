199c78492d71a6fbdbd596d1b06e865c168206fe
Wil DAmato <wil.damato@gmail.com>
2015-12-12 20:49:49 +0000

    add data, more bids, max bid function

--- tbay.py
+++ tbay.py
@@ -10,7 +10,7 @@ Base = declarative_base()
 
 
 from datetime import datetime
-
+from sqlalchemy.sql.expression import func 
 from sqlalchemy import Column, Integer, String, DateTime, Float, ForeignKey
 from sqlalchemy.orm import relationship
 
@@ -23,7 +23,7 @@ class Item(Base):
     description = Column(String)
     start_time = Column(DateTime, default=datetime.utcnow)
     
-    bids = relationship("Bid", backref="bids")
+    bids = relationship("Bid", backref="item")
     
     user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     
@@ -49,28 +49,42 @@ class Bid(Base):
   item_id = Column(Integer, ForeignKey('items.id'), nullable=False)
   
 
-"""
+Base.metadata.create_all(engine)
+
 # Users
 elsa = User(username="Elsa", password="1d1n4")
 anna = User(username="Anna", password="b3ll")
 kristoff = User(username="Kristoff", password="gr0ff")
+session.add_all([elsa, anna, kristoff])
+session.commit()
 
 # Items
 olaf = Item(name="Olaf", description="snowman", user_id=1)
 sven = Item(name="Sven", description="reindeer", user_id=3)
 marshmallow = Item(name="Marshmallow", description="snow monster", user_id=2)
 baseball = Item(name="baseball", description="rawhide", user_id=3)
+session.add_all([olaf, sven, marshmallow, baseball])
+session.commit()
 
 # Bids
-bid1 = Bid(price=50.0, user_id=1, item_id=4)
-bid2 = Bid(price=67.0, user_id=2, item_id=4)
+bid1 = Bid(price=50.0, user_id=elsa.id, item_id=baseball.id)
+bid2 = Bid(price=67.0, user_id=anna.id, item_id=baseball.id)
+bid3 = Bid(price=80.0, user_id=kristoff.id, item_id=baseball.id)
+bid4 = Bid(price=27.0, user_id=elsa.id, item_id=baseball.id)
+bid5 = Bid(price=101.0, user_id=anna.id, item_id=baseball.id)
+bid6 = Bid(price=99.0, user_id=kristoff.id, item_id=baseball.id)
+session.add_all([bid1, bid2, bid3, bid4, bid5, bid6])
+session.commit()
 
 
+records = session.query(Bid).all()
 
+def GetMaxBidRecord(records):
+  max_bid = records[0]
+  for record in records:
+      if record.price > max_bid.price:
+        max_bid = record
+  return max_bid
 
-
-"""
-
-
-Base.metadata.create_all(engine)
-
+max_bid = GetMaxBidRecord(session.query(Bid).all())
+print(max_bid.bidder.username, max_bid.price)
